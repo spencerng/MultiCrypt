03edcfc7651c4cc3039570b11835cdf2d120391c
Spencer Ng <sng1488@gmail.com>
2016-03-03 15:11:36 -0500

    Added mathematical functions
    Added functions for transpose, cofactors, cofactor matrix, adjunct,
    converting an int matrix to a double matrix, and inverse.
    
    Minor matrix function changed for any row and column value.
    
    Function declarations missing and all new functions are untested. A new
    section in the main function will be needed to test the functions,
    partially created.

--- Cryptography Project/Cryptography.cpp
+++ Cryptography Project/Cryptography.cpp
@@ -6,9 +6,9 @@
 #include <ctime>
 using namespace std;
 
-//Functions used for general matrix multiplication
+//Functions used for general matrix operations
 	
-	vector< vector<int> > toMMatrix(vector< vector<int> > A, int column);
+	vector< vector<int> > toMMatrix(vector< vector<int> > A, int row, int column);
 	// Creates the minor matrix of A across the first row and a given column
 	// Used in the det and toCofMatrix functions
 
@@ -19,18 +19,23 @@ using namespace std;
 	vector< vector<int> > multiply(vector< vector<int> > A, vector< vector<int> > B);
 	// Returns the product of a m x n matrix and a n x m matrix. Used to encrypt and decrypt messages
 
+	vector< vector<int> > scalarMultiply(int scalar, vector < vector<int> >A);
+	// Used to multiply a m x n matrix by a scalar;
+	// Written but not tested
+
 	vector< vector<int> > transpose(vector< vector<int> > A);
-	// Returns the transpose of a m x n matrix, has not been written yet. Will be used in the toAdjMatrix function.
+	// Returns the transpose of a m x n matrix. Will be used in the toAdjMatrix function.
+	// Written but not tested
 
 	vector < vector<int > > toCofMatrix(vector< vector<int> > A);
 	// Returns the cofactor matrix of a given matrix, utilizing determinant and minor matrix with a definition for a 2 x 2.
-	// Used for the adjugate matrix, not yet written.
+	// Used for the adjugate matrix. Written but not tested.
 
 	vector< vector<int> > toAdjMatrix(vector< vector<int> > A);
-	// Calculates the adjugate matrix, or the transpose of the cofactor matrix. Not yet written.
+	// Calculates the adjugate matrix, or the transpose of the cofactor matrix.  Written but not tested
 
-	vector< vector<int> > inverse(vector< vector<int> > A);
-	// Calculates the inverse of a matrix, equal to Adj(A) * (1/det(A)). Not yet written.
+	vector< vector<double> > inverse(vector< vector<int> > A);
+	// Calculates the inverse of a matrix, equal to Adj(A) * (1/det(A)).  Written but not tested
 
 
 vector< vector<int> > randomMatrix(int size);
@@ -46,6 +51,7 @@ vector<char> stringToCharVec(string input);
 // Converts a string to a vector filled with char entries. Has not been tested.
 
 void printMatrix(vector< vector<int> > A);
+void printMatrix(vector< vector<double> > A);
 // Prints any given matrix, with formatting.
 
 //Further functions will be needed for file I/O. Main function will need a good user interface with additional features as necessary.
@@ -53,12 +59,15 @@ void printMatrix(vector< vector<int> > A);
 int main() {
 	
 	while (true) {
+		
+
 		cout << "(1) Encode a message\n";
 		cout << "(2) Decode a message\n";
 		cout << "(3) Generate an encryption key\n";
 		cout << "(4) Generate a decryption key\n";
-		cout << "(5) Exit the program";
-		cout << "(6) Calculate the determinant of a matrix\n";
+		cout << "(5) Exit the program\n";
+		cout << "(6) Multiply two matrices\n";
+		cout << "(7) Debug\n";
 		char choice;
 		cin >> choice;
 		system("cls");
@@ -68,6 +77,18 @@ int main() {
 		
 		if (choice == '1')
 		{
+		}
+		if (choice == '7') {
+			cout << "Enter the size of an n x n matrix: ";
+			cin >> sizeI;
+			vector< vector<int> > debugMatrix(sizeI, vector<int>(sizeI));
+			cout << "\nEnter the " << sizeI << " x " << sizeI << " matrix.\n";
+			
+			for (int i = 0; i < sizeI; i++)
+				for (int j = 0; j < sizeI; j++)
+					cin >> debugMatrix[i][j];
+
+
 		}
 		if (choice == '6') {
 			cout << "Enter the size of the m x n matrix A: ";
@@ -114,43 +135,7 @@ int main() {
 		}
 		if (choice == '4') {
 
-			int element;
-			cout << "Please enter the number of columns in your encryption key: ";
-			cin >> size;
-			cout << "\n Enter your encryption key, in " << size << " rows of " << size << " numbers, each followed by a space.\n";
-			vector< vector<int> > encryptionMatrix;
-			for (int i = 0; i < size; i++) {
-				for (int j = 0; j < size; j++) {
-					cin >> element;
-					encryptionMatrix[i][j] = element;
-				}
 
-			}
-
-			vector< vector<int> > decryptionMatrix = inverse(encryptionMatrix);
-			system("cls");
-
-			cout << "Decryption Matrix: \n\n";
-			for (int i = 0; i < size; i++) {
-				for (int j = 0; j < size; j++) {
-					
-					cout << decryptionMatrix[i][j] << " ";
-				}
-				cout << endl;
-			}
-
-			do {
-				cout << "\nWould you like to decrypt a matrix using this key? (Y/N)\n\n";
-				cin >> choice;
-				if (choice != 'y' && choice != 'Y' && choice != 'n' && choice != 'N')
-					cout << "Invalid choice. Please try again.\n";
-				system("pause");
-				system("cls");
-
-
-			} while (choice != 'y' && choice != 'Y' && choice != 'n' && choice != 'N');
-			if (choice == 'n' || choice == 'N')
-				;
 		}
 		if (choice == '2') {
 			string encryptedMessage;
@@ -170,20 +155,29 @@ int main() {
 	return 0;
 }
 
-vector< vector<int> > toMMatrix(vector< vector<int> > A, int column) {
+vector< vector<int> > toMMatrix(vector< vector<int> > A, int row, int column) {
 	int m = 0, n = 0;
 	vector< vector<int> > mMatrix(A.size() - 1, vector<int>(A.size() - 1));
 	for (int i = 1; i < A.size(); i++) {
-		for (int j = 0; j < A.size(); j++) {
-			if (j != column) {
-				mMatrix[m][n] = A[i][j];
-				n++;
+		if (i != row) {
+			for (int j = 0; j < A.size(); j++) {
+				if (j != column) {
+					mMatrix[m][n] = A[i][j];
+					n++;
+				}
 			}
+			m++;
+			n = 0;
+
 		}
-		m++;
-		n = 0;
+		
 	}
 	return mMatrix;
+}
+
+int cofactor(vector< vector<int> > A, int row, int column) {
+	int cof = pow(-1, row + column + 2) * det(toMMatrix(A, row, column));
+	return cof;
 }
 
 int det(vector< vector<int> > A) {
@@ -193,7 +187,7 @@ int det(vector< vector<int> > A) {
 		determinant = A[0][0] * A[1][1] - A[0][1] * A[1][0];
 
 	else for (int j = 0; j < A.size(); j++)
-		determinant += pow(-1, j + 2)*A[0][j] * (det(toMMatrix(A, j)));
+		determinant += A[0][j] * cofactor(A, 0, j);
 
 
 	return determinant;
@@ -209,14 +203,6 @@ vector< vector<int> > multiply(vector< vector<int> > A, vector< vector<int> > B)
 				productMatrix[i][j] += A[i][m] * B[m][j];
 
 	return productMatrix;
-}
-
-vector< vector<int> > inverse(vector< vector<int> > A) {
-	vector< vector<int> > inverseMatrix(A.size(), vector<int>());
-
-	//Needs to be done, will be based on rref
-
-	return inverseMatrix;
 }
 
 vector< vector<int> > randomMatrix(int size) {
@@ -268,3 +254,74 @@ void printMatrix(vector< vector<int> > A) {
 	}
 
 }
+
+void printMatrix(vector< vector<double> > A) {
+	for (int i = 0; i < A.size(); i++) {
+		for (int j = 0; j < A[0].size(); j++)
+			cout << setw(3) << A[i][j];
+		cout << endl;
+	}
+
+}
+
+vector< vector<int> > transpose(vector< vector<int> > A) {
+	vector< vector<int> > transMatrix(A[0].size(), vector<int>(A.size()));
+	for (int i = 0; i < transMatrix.size(); i++)
+		for (int j = 0; j < transMatrix[0].size(); j++)
+			transMatrix[i][j] = A[j][i];
+	return transMatrix;
+}
+
+vector < vector<int > > toCofMatrix(vector< vector<int> > A) {
+	vector< vector<int> > cofMatrix(A.size(), vector<int>(A[0].size()));
+
+	for (int i = 0; i < cofMatrix.size(); i++)
+		for (int j = 0; j < cofMatrix[0].size(); j++)
+			cofMatrix[i][j] = cofactor(A, i, j);
+
+	return cofMatrix;
+}
+
+vector< vector<double> > scalarMultiply(int scalar, vector < vector<double> >A) {
+	vector< vector<double> > product = A;
+	for (int i = 0; i < product.size(); i++)
+		for (int j = 0; j < product[0].size(); j++)
+			product[i][j] *= scalar;
+	return product;
+
+}
+
+vector< vector<int> > toAdjMatrix(vector< vector<int> > A) {
+
+	return transpose(toCofMatrix(A));
+
+}
+
+vector< vector<double> > toFloatMatrix(vector < vector<int> >A) {
+	vector< vector<double> > result(A.size(), vector<double>(A[0].size()));
+	for (int i = 0; i < A.size(); i++)
+		for (int j = 0; i < A.size(); j++)
+			result[i][j] = (double)A[i][j];
+	return result;
+
+}
+
+vector< vector<double> > inverse(vector< vector<int> > A) {
+	vector< vector<double> > result = toFloatMatrix(A);
+
+	if (A.size() == 2) {
+		result[0][0] = A[1][1];
+		result[0][1] = -1 * A[0][1];
+		result[1][0] = -1 * A[1][0];
+		result[1][1] = A[0][0];
+	}
+	else result = toFloatMatrix(toAdjMatrix(A));
+
+	result = scalarMultiply((1 / det(A)), result);
+
+	return result;
+
+}
+
+
+
