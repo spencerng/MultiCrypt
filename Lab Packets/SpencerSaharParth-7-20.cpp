995bf214af2cb95c6d1396a56363d76d0e307194
Spencer N <sng1488@gmail.com>
2016-03-18 16:49:12 -0400

    Added lab packets

---
+++ Lab Packets/SpencerSaharParth-7-20.cpp
@@ -0,0 +1,29 @@
+// ( Airline Reservations System) A small airline has just purchased a computer for its
+// new automated reservations system. You have been asked to program the new
+// system. You are to write a program to assign seats on each flight of the airline's only
+// plane (capacity: 10 seats).
+// Your program should display the following menu of alternativesPlease type 1 for
+// "First Class" and Please type 2 for "Economy". If the person types 1, your program
+// should assign a seat in the first class section (seats 1-5). If the person types 2, your
+// program should assign a seat in the economy section (seats 6-10). Your program
+// should print a boarding pass indicating the person's seat number and whether it is in
+// the first class or economy section of the plane.
+// Use a one-dimensional array to represent the seating chart of the plane. Initialize all
+// the elements of the array to 0 to indicate that all seats are empty. As each seat is
+// assigned, set the corresponding elements of the array to 1 to indicate that the seat
+// is no longer available.
+// Your program should, of course, never assign a seat that has already been assigned.
+// When the first class section is full, your program should ask the person if it is
+// acceptable to be placed in the economy section (and vice versa). If yes, then make
+// the appropriate seat assignment. If no, then print the message "Next flight leaves
+// in 3 hours". 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	
+	
+	return 0;
+}
