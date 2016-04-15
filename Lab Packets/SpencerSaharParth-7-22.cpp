90b20875a6f736b411ff3a635c2d3f1b9662c708
Parth7350 <psavla2@gmail.com>
2016-04-14 22:34:38 -0400

    Update SpencerSaharParth-7-22.cpp

--- Lab Packets/SpencerSaharParth-7-22.cpp
+++ Lab Packets/SpencerSaharParth-7-22.cpp
@@ -17,11 +17,65 @@
 // to the right of the totaled rows and to the bottom of the totaled columns. 
 
 #include<iostream>
+#include<vector>
+#include<iomanip>
 using namespace std;
 
-int main(){
-	
-	
-	
+class Salesperson {
+
+public:
+	double sales[5];
+	double totalSales;
+	void sellItem(int itemNumber, double value) {
+		sales[itemNumber - 1] += value;
+	}
+
+	void sumSales() {
+		totalSales = 0;
+		for (int i = 0; i < 5; i++)
+			totalSales += sales[i];
+	}
+};
+
+int main() {
+	vector<Salesperson> sales(4, Salesperson());
+	char choice;
+	int salesNumber, productNumber;
+	double value;
+	do {
+		cout << "What is the salesperson number? (1-4)\n";
+		cin >> salesNumber;
+		cout << "\nWhat is the product number? (1-5)\n";
+		cin >> productNumber;
+		cout << "\nWhat is the total dollar value of product " << productNumber << " sold that day?\n";
+		cin >> value;
+
+		sales[salesNumber - 1].sellItem(productNumber, value);
+
+		cout << "\nWould you like to create a new salesslip? (Y/N)\n";
+		cin >> choice;
+		system("cls");
+	} while (tolower(choice) == 'y');
+
+	for (int i = 0; i < 4; i++)
+		sales[i].sumSales();
+
+	cout << "\t     ";
+	for (int i = 1; i <= 4; i++)
+		cout << setw(15) << "Salesperson " << i << " | ";
+	cout << endl;
+	for (int product = 0; product<5; product++) {
+		cout << "Product " << product + 1 << ": |";
+		double colSales = 0;
+		for (int person = 0; person < 4; person++) {
+			cout << setw(12) << "$" << setprecision(2) << fixed << sales[person].sales[product] << "  |";
+			colSales += sales[person].sales[product];
+		}
+		cout << " $" << colSales << endl;
+	}
+	cout << "\t\t";
+	/*for (int i = 0; i <4; i++)
+		cout << setw(8) << setprecision(2) << fixed << "$" << sales[i].totalSales << " | ";
+	cout << endl;*/
 	return 0;
 }
